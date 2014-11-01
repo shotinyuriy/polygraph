d97b72f7a47dd99cd839a8260a328b6392b1dde3
shotinyuriy <shotinyuriy@gmail.com>
2014-11-01 12:46:53 +0400

    employee type improve

---
+++ polygraph-desktop-fx/src/main/java/kz/aksay/polygraph/entityfx/EmployeeTypeFX.java
@@ -0,0 +1,12 @@
+package kz.aksay.polygraph.entityfx;
+
+import kz.aksay.polygraph.entity.EmployeeType;
+
+public class EmployeeTypeFX {
+
+	private EmployeeType employeeType;
+	
+	public EmployeeTypeFX(EmployeeType employeeType) {
+		this.employeeType = employeeType;
+	}
+}
